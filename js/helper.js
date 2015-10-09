5bff4c9555ae0e84ca0d1d0255a4dbdd0c5612db
Paul Matencio <paulo.matencio@gmail.com>
2015-10-09 07:27:13 +0200

    Trying to commit

--- js/helper.js
+++ js/helper.js
@@ -6,8 +6,6 @@ Don't worry, you'll learn what's going on in this file throughout the course. Yo
 
 Cameron Pittman
 */
-
-
 /*
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
@@ -31,6 +29,7 @@ var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
+
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
@@ -55,7 +54,8 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var internationalizeButton = '<button class="button">Internationalize</button>';
+
 var googleMap = '<div id="map"></div>';
 
 
@@ -63,10 +63,13 @@ var googleMap = '<div id="map"></div>';
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
+    $('button').click(function() {
+        var iName = inName(bio.name) || function() {};
+        $('#name').html(iName);
+        /* d3.selectAll("h1").style("color", "white"); */
+        d3.selectAll(".work-entry").style("font-family", "");
+
+    });
 });
 
 /*
@@ -74,18 +77,32 @@ The next few lines about clicks are for the Collecting Click Locations quiz in L
 */
 clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
+function logClicks(x, y) {
+    clickLocations.push({
+        x: x,
+        y: y
+    });
+    // console.log('x location: ' + x + '; y location: ' + y);
 }
 
+/*
+var fclick = function(loc) {
+   logClicks(loc.pageX,loc.pageY)
+}
+$(document).click(fclick) ;
+
+  Below is the same code as ablove but we are passing an javascript anonymous fuction  to the click argument => function without name and are oftent returned by other
+  functions or objects
+
+  The  example is
+  $.getJSON("test.json",function(data){ console.log(data)})
+*/
+
 $(document).click(function(loc) {
-  // your code goes here!
+    // your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x, y);
 });
 
 
@@ -95,139 +112,149 @@ This is the fun part. Here's where we generate the custom Google Map for the web
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
 Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
+    var locations;
+    var mapOptions = {
+        disableDefaultUI: true
+    };
 
-  var locations;
-
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    /*
+    For the map to be displayed, the googleMap var must be
+    appended to #mapDiv in resumeBuilder.js.
+    */
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
+    /*
+    locationFinder() returns an array of every location string from the JSONs
+    written for bio, education, and work.
+    */
+    function locationFinder() {
+        // initializes an empty array
+        var locations = [];
+        // adds the single location property from bio to the locations array
+        locations.push(bio.contacts.location);
+        // iterates through school locations and appends each location to
+        // the locations array
+        for (var school in education.schools) {
+            locations.push(education.schools[school].location);
+        }
+        // iterates through work locations and appends each location to
+        // the locations array
+        for (var job in work.jobs) {
+            locations.push(work.jobs[job].location);
+        }
 
-    // initializes an empty array
-    var locations = [];
+        for (var proj in project.projects) {
+            locations.push(project.projects[proj].location);
+        }
 
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+        return locations;
+    }
 
-    // iterates through school locations and appends each location to
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat(); // latitude from the place service
+        var lon = placeData.geometry.location.lng(); // longitude from the place service
+        var name = placeData.formatted_address; // name of the place from the place service
+
+        var trimname = name.split(",")[0].trim();
+        var mymarker = mapmaker[trimname]
+        if (mymarker != null) {
+            var icon = mymarker.icon;
+            var image = mymarker.image;
+        } else {
+            console.log(name, "Missing images")
+        }
+
+
+        var bounds = window.mapBounds; // current boundaries of the map window
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            icon: icon,
+            title: name
+        });
+
+        // infoWindows are the little helper windows that open when you click
+        // or hover over a pin on a map. They usually contain more information
+        // about a location.
+        var content = name;
+        //var image = project.getImage(content.split(",")[0].trim()) ;
+        // var image = mapmaker[content.split(",")[0].trim()];
+        if (image != null) {
+            content = "<div><img src='" + image + "' height='32' width='32' alt='City image'></div>"
+        }
+        var infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+
+        // hmmmm, I wonder what this is about...
+        google.maps.event.addListener(marker, 'click', function() {
+            // your code goes here!
+            //infoWindow.content="<div><img src='images/Rice-cutting-4.jpg' height='64' width='42'>></div>"
+            infoWindow.open(map, marker);
+
+
+        });
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+        map.setCenter(bounds.getCenter());
+    } // end CreateMapMarker
+
+    /*
+    callback(results, status) makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            //console.log(results[0]);
+            createMapMarker(results[0]);
+        }
+    }
+
+    /*
+    pinPoster(locations) takes in the array of locations created by locationFinder()
+    and fires off Google place searches for each location
+    */
+    function pinPoster(locations) {
+        // creates a Google place search service object. PlacesService does the work of
+        // actually searching for location data.
+        var service = new google.maps.places.PlacesService(map);
+        // Iterates through the array of locations, creates a search object for each location
+        for (var place in locations) {
+            // the search request object
+            var request = {
+                query: locations[place]
+            };
+            // Actually searches the Google Maps API for location data and runs the callback
+            // function with the search results after each search.
+            service.textSearch(request, callback);
+        }
+    }
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
+    // locations is an array of location strings returned from locationFinder()
+    locations = locationFinder();
+    // pinPoster(locations) creates pins on the map for each location in
     // the locations array
-    for (var school in education.schools) {
-      locations.push(education.schools[school].location);
-    }
-
-    // iterates through work locations and appends each location to
-    // the locations array
-    for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
-    }
-
-    return locations;
-  }
-
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
-
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
-    for (var place in locations) {
-
-      // the search request object
-      var request = {
-        query: locations[place]
-      };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
-    }
-  }
-
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
-
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
-
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
-
+    pinPoster(locations);
 }
 
 /*
@@ -235,11 +262,14 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+
+window.addEventListener('resize', function(e) {
+    //Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
+
+
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,318 @@
+var welcomeMessage = "Hello, here are my bio:";
+
+var bio = {
+    "name": "Patrick Vauban",
+    "role": "Happy man",
+    "contacts": {
+        "email": "patrick.vauban@gmail.com",
+        "mobile": "+3161894564",
+        "twitter": "@patrickv",
+        "github": "PatrickVauban",
+        "location": "The Hague, The Netherlands"
+    },
+    "skills": ["Active listening", "Taking responsability", "Humor", "Creativity", "Negotiation"],
+    "biopic": "images/fry.jpg"
+};
+
+var education = {
+    "schools": [{
+        "name": "Street University",
+        "location": "Saigon, Vietnam",
+        "url": "https://www.google.com/maps/d/viewer?mid=zM0A9j5HAmUs.k5zX5AIxoZII&hl=en",
+        "degree": "Drinking green tea",
+        "dates": "1900-1960",
+        "minor": [
+            "Vietnamese"
+        ],
+        "major": [
+            "Master of tea"
+        ]
+    }, {
+        "name": "Life University",
+        "location": "Savannakhet, Laos",
+        "url": "https://www.youtube.com/watch?v=E5AKmTsBCgQ",
+        "degree": "Eating glutinous rice",
+        "dates": "1960-1970",
+        "minor": [
+            "French",
+            "English",
+            "Lao"
+        ],
+        "major": [
+            "rice planter"
+        ]
+    }, {
+        "name": "Home University",
+        "location": "The Hague, The Netherlands",
+        "url": "http://denhaag.com/en",
+        "degree": "Hanging pictures",
+        "dates": "1970-1978",
+        "minor": [
+            "no degree"
+        ],
+        "major": [
+            "No degree"
+        ]
+    }, ],
+    "online": [{
+        "date": 2015,
+        "minor": "Web Developer"
+    }]
+}
+
+var work = {
+    "jobs": [{
+        "employer": "Lam dong tea joint stock company",
+        "url": "http://www.hktdc.com/manufacturers-suppliers/LAM-DONG-TEA-JOINT-STOCK-COMPANY/en/1X06I38J",
+        "title": "Sales support",
+        "dates": "1900-1920",
+        "location": "Lâm Đồng, Vietnam",
+        "description": "Handle accounts, Income Tax and agricultural income tax. Cost control. File Tea Board returns.banking including submission of proposal and monthly reports.Preparation of MIS reports for Hanoi Office. Assisting in Sales and its documentation and monitoring realisations in case of private sales, purchases, correspondence. "
+    }, {
+        "employer": "Our-company Homechampa",
+        "url": "http://www.homchampa.com/our-company",
+        "title": "Rice planter assistant",
+        "dates": "1940-1960",
+        "location": "Pakse, Laos",
+        "description": "Clearing Land as Part of the Slash-and-Burn Process; Burning; Preparing the Land for Sowing; Sowing; Weeding; Scaring the Birds Away; Harvest; Threshing, Winnowing and Storage. "
+    }, {
+        "employer": "MyBoss galery",
+        "url": "http://www.nowhere.org",
+        "title": "Galery Assistant",
+        "dates": "1980-current",
+        "location": "The Hague, Netherlands",
+        "description": "Put frames and hang the paintings on wall without nails. Adhesive picture hanging strips require a clean surface to stick properly, so wipe down your picture and the wall where you’re going to hang it with a clean cloth and isopropyl alcohol. "
+    }]
+};
+
+var project = {
+    "projects": [
+
+        {
+            "title": "Preparing tea",
+            "dates": "1900-1920",
+            "description": "Preparing tea properly is easy but you must follow a few basic guidelines. The best tea in the world can taste horrible if prepared incorrectly, and conversely a very inexpensive tea can be very satisfying if made well. All TeaSource tea comes with specific steeping suggestions on the back of the bag, but some general guidelines are listed below.There are seven factors that effect the quality of brewed tea: The quality of the tea; The quality of the water; Correct measurement; Correct steeping temperature; Correct steeping time; Allowing the tea leaf to expand fully; Separating the leaf from the liquid at the end of the steeping process",
+            "url": "http://www.asian-recipe.com/china/preparing-chinese-tea.html",
+            "location": "Hanoï, Vietnam",
+            "images": ["images/Tea-preparation-1.jpg", "images/Tea-preparation-2.jpg", "images/Tea-preparation-3.jpg"]
+        }, {
+            "title": "Harvesting rice",
+            "dates": "1940-1960",
+            "description": "Harvesting rice consists of the basic operations cutting, threshing, and cleaning. Cutting is the first operation in harvesting. Depending on the crop's condition, and availability of labor or machinery, cutting can be done either manually or mechanically. However, due to severe labor shortage and the resulting increase in harvesting cost, the use of combines is increasing rapidely and economically attractive.",
+            "url": "http://www.infoplease.com/encyclopedia/science/rice-cultivation-harvesting.html",
+            "location": "Savannakhet, Laos",
+            "images": ["images/Rice-cutting-0.jpg", "images/Rice-cutting-2.jpg", "images/Rice-cutting-3.jpg", "images/Rice-cutting-4.jpg"]
+        }, {
+            "title": "Hanging pictures",
+            "dates": "1980-current",
+            "url": "http://www.holland.com/us/tourism/cities/the-hague-1/museums-in-the-hague-1.htm",
+            "description": "The operation consist to hang pcitures without nail: Put up and take down pictures and other objects as often as you want, without the fear of marking or damaging walls. Hang picture frames, canvasses, artwork or decorative items - straight and level. Each pair of small strips holds up to 450 g. Each pair of medium strips holds up to 1 Kg. Dimensions of strips: small = 54 x 16 x 2 mm / medium = 70 x 19 x 2 mm",
+            "location": "La Haye, The Netherlands",
+            "images": ["images/IMG_20151004_114935-320.jpg", "images/IMG_20151004_114801-320.jpg", "images/IMG_20151004_114816-320.jpg", "images/IMG_20151004_115045-320.jpg", "images/IMG_20151004_115123-320.jpg"]
+        }
+    ]
+}
+
+var mapmaker = {
+    "La Haye": {
+        "icon": "images/homeMarker.png",
+        "image": "images/TheHague.jpg"
+    },
+    "The Hague": {
+        "icon": "images/homeMarker.png",
+        "image": "images/TheHague.jpg"
+    },
+    "Savannakhet": {
+        "icon": "images/purpleMarker.png",
+        "image": "images/Savannakhet.jpg"
+    },
+    "Hanoï": {
+        "icon": "images/purpleMarker.png",
+        "image": "images/Hanoi.jpg"
+    },
+    "Lâm Đồng": {
+        "icon": "images/blueMarker.jpg",
+        "image": "images/Lamdong.jpg"
+    },
+    "Hô Chi Minh-Ville": {
+        "icon": "images/purpleMarker.png",
+        "image": "images/saigon.jpg"
+    },
+    "Hô Chi Minh-City": {
+        "icon": "images/purpleMarker.png",
+        "image": "images/saigon.jpg"
+    },
+    "Paksé": {
+        "icon": "images/blueMarker.jpg",
+        "image": "images/Parkse.jpg"
+    },
+    "Pakse": {
+        "icon": "images/blueMarker.jpg",
+        "image": "images/Parkse.jpg"
+    }
+}
+
+
+/* useful functions */
+function displayMap() {
+    $("#mapDiv").append(googleMap);
+}
+
+/* function inName() is used in helper.js */
+function inName(myname) {
+    name1 = myname.trim().split(" ");
+    name1[1] = name1[1].toUpperCase();
+    name1[0] = name1[0].slice(0, 1).toUpperCase() + name1[0].slice(1).toLowerCase();
+    return name1[1] + " " + name1[0];
+}
+
+
+function displayWelcome() {
+    $("#welcome").prepend(HTMLwelcomeMsg.replace("%data%", welcomeMessage));
+}
+
+function mySkills() {
+    $("#header").append(HTMLskillsStart);
+    for (var skill in bio.skills) {
+        $("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
+    }
+}
+
+bio.display = function display() {
+    $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+    $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+    $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+    $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+    $("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+    $("#topContacts").append(HTMLblog.replace("%data%", bio.contacts.github));
+    mySkills();
+}
+
+
+function displayInternationalizeButton() {
+    $("#main").prepend(internationalizeButton);
+}
+
+/*  Work section of the  Resume */
+work.PutOnResume = function(index) {
+    // Create a  new div for ecah work
+    $("#workExperience").append(HTMLworkStart);
+    // concat employer and title
+    var employeur = HTMLworkEmployer.replace("%data%", work.jobs[index].employer);
+    employeur = employeur.replace("#", work.jobs[index].url);
+    title = HTMLworkTitle.replace("%data%", work.jobs[index].title);
+    $(".work-entry:last").append(employeur + title);
+    //concat dates
+    $(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[index].dates));
+    //concat location and country and description
+    $(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[index].location));
+    $(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[index].description));
+}
+
+work.display = function display() {
+    for (var job in work.jobs.reverse()) {
+        work.PutOnResume(job);
+    }
+}
+
+work.locationizer = function() {
+    /*function locationizer(work) { */
+    var locationArray = []
+    for (var job in work.jobs.reverse()) {
+        var location = work.jobs[job].location;
+        locationArray.push(location);
+    }
+    return locationArray;
+}
+
+
+project.getImage = function(name) {
+    for (proj in project.projects) {
+        // console.log(name, project.projects[proj].location);
+        if (project.projects[proj].location.search(name) >= 0) {
+            // console.log("hit");
+            return project.projects[proj].images[0];
+        }
+    }
+    return null;
+}
+
+/* Project section of the Resume */
+var HTMLRow = '<div class="row">';
+var HTMLCol = '<div class="%data%">';
+var HTMLEndDiv = "</div>"
+
+project.PutOnResume = function(index) {
+    $("#projects").append(HTMLprojectStart);
+    var title = HTMLprojectTitle.replace("%data%", project.projects[index].title);
+    $(".project-entry:last").append(title.replace("#", project.projects[index].url));
+    $(".project-entry:last").append(HTMLprojectDates.replace("%data%", project.projects[index].dates));
+    /* $(".project-entry:last").append(HTMLprojectLocation.replace("%data%",project.project[index].location)); */
+    $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", project.projects[index].description));
+    if (project.projects[index].images.length > 0) {
+        images = HTMLRow;
+        for (img in project.projects[index].images) {
+            images = images + HTMLCol.replace("%data%", "col-2") +
+                HTMLprojectImage.replace("%data%", project.projects[index].images[img]) + HTMLEndDiv;
+        }
+        images = images + HTMLEndDiv + HTMLEndDiv;
+        $(".project-entry:last").append(images);
+    }
+}
+
+project.display = function() {
+        for (var proj in project.projects.reverse()) {
+            /* putProjectOnResume(proj); */
+            project.PutOnResume(proj);
+        }
+    }
+    /* Education section of the Resume */
+
+
+education.putOnresume = function(index) {
+    $("#education").append(HTMLschoolStart);
+    var school = HTMLschoolName.replace("%data%", education.schools[index].name);
+    // console.log(school);
+    $(".education-entry:last").append(school.replace("#", education.schools[index].url));
+    $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", education.schools[index].degree));
+    $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[index].dates));
+    $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[index].location));
+    $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[index].major));
+}
+
+education.display = function() {
+    for (var educ in education.schools.reverse()) {
+        education.putOnresume(educ);
+    }
+}
+
+
+/* map API */
+/* d3 API */
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var paragraphs = document.getElementsByTagName("p");
+for (var i = 0; i < paragraphs.length; i++) {
+  var paragraph = paragraphs.item(i);
+  paragraph.style.setProperty("color", "white", null);
+} can be replaced by
+ d3.selectAll("p").style("color", "white");
+*/
+
+
+/*  DISPLAY the RESUME */
+displayInternationalizeButton();
+displayMap();
+displayWelcome();
+
+bio.display();
+work.display();
+project.display();
+education.display();
+
+
+
+
+
+
+
